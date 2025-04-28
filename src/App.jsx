67e423edf42e6427ecae61350aad3ceef19aa95e
RohitSah23 <rohitisog@gmail.com>
2025-04-28 15:19:45 +0530

    feat(why blockchain) : add svg background

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Hero from "./components/Hero/Hero";
 import Companies from "./components/Companies/Companies";
+import WhyBlockchain from "./components/WhyBlockchain/WhyBlockchain";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
 
       <Hero/>
       <Companies/>
+      <WhyBlockchain/>
     </>
   );
 };
