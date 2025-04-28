34a42edf63d36a78fdcc0b1b2c23d455c468b7e8
RohitSah23 <rohitisog@gmail.com>
2025-04-28 15:20:58 +0530

    fix(why blockchain) : svg bug fix

--- src/components/WhyBlockchain/WhyBlockchain.jsx
+++ src/components/WhyBlockchain/WhyBlockchain.jsx
@@ -1,19 +1,9 @@
 import React from "react";
-import SVGBackground from "../../assets/SVG";
 
 const WhyBlockchain = () => {
   return (
     <div className="min-h-screen">
-      <svg
-        style={{ width: '100%', height: '100%' }}
-        viewBox="0 0 1439 834"
-        preserveAspectRatio="none"
-        width="100%"
-        height="100%"
-      >
-        <use href="#svg-654112387_32560"></use>
-      </svg>
-      <SVGBackground className="h-screen" />
+     
     </div>
   );
 };
